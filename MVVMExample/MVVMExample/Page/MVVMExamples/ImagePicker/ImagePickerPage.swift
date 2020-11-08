c521705da42fd658fe37c58c4e1d96995df12800
Pham Minh Tien <phamminhtien305@gmail.com>
2020-11-08 22:12:25 +0700

    Update create post screen

--- MVVMExample/MVVMExample/Page/MVVMExamples/ImagePicker/ImagePickerPage.swift
+++ MVVMExample/MVVMExample/Page/MVVMExamples/ImagePicker/ImagePickerPage.swift
@@ -12,14 +12,26 @@ import RxCocoa
 import RxSwift
 
 class ImagePickerPage: BasePage {
+
+    @IBOutlet private weak var titleTxt: UITextField!
+    @IBOutlet private weak var descLb: UITextView!
+    @IBOutlet private weak var photoImg: UIImageView!
+    
+    @IBOutlet private weak var photoBtn: UIButton!
+    @IBOutlet private weak var localBtn: UIButton!
+    @IBOutlet private weak var cameraBtn: UIButton!
+    
     let addBtn = UIBarButtonItem(barButtonSystemItem: .add, target: nil, action: nil)
+    
+    @IBOutlet private weak var actackViewHeight: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         enableBackButton = true
-        navigationItem.rightBarButtonItem = addBtn
+        addBtn.title = "Post"
+        self.navigationItem.rightBarButtonItem = addBtn
     }
 
     override func initialize() {
@@ -31,8 +43,39 @@ class ImagePickerPage: BasePage {
     override func bindViewAndViewModel() {
         super.bindViewAndViewModel()
         guard let viewModel = self.viewModel as? ImagePickerViewModel else { return }
+        addBtn.rx.bind(to: viewModel.postAction, input: ())
         
-        addBtn.rx.tap
+        viewModel.rxTitle <~> titleTxt.rx.text => disposeBag
+        viewModel.rxDescription <~> descLb.rx.text => disposeBag
+        
+        viewModel.rxImage.subscribe(onNext: {[weak self] image in
+            if image == nil {
+                self?.actackViewHeight.constant = 0
+            } else {
+                self?.actackViewHeight.constant = 80
+            }
+            self?.photoImg.image = image
+        }) => disposeBag
+        /// Or
+        /*
+        viewModel.rxImage.subscribe { (img) in
+            switch img {
+            case let .next(image):
+                if image == nil {
+                    self.actackViewHeight.constant = 0
+                } else {
+                    self.actackViewHeight.constant = 80
+                }
+            case .error(_):
+                ()
+            case .completed:
+                ()
+            }
+        } => disposeBag
+         */
+        
+        
+        photoBtn.rx.tap
             .flatMapLatest { [weak self]_ in
                 return UIImagePickerController.rx.createWithParent(self, animated: true) { picker in
                     picker.sourceType = .photoLibrary
@@ -44,5 +87,10 @@ class ImagePickerPage: BasePage {
                 return info[UIImagePickerController.InfoKey.originalImage.rawValue] as? UIImage
             }
             .bind(to: viewModel.rxImage) => disposeBag
+        
+        viewModel.rxCanPost.subscribe { (enable) in
+            print("rxCanPost: Enable \(enable)")
+        } => disposeBag
+
     }
 }
--- MVVMExample/MVVMExample/Page/MVVMExamples/ImagePicker/ViewModel/ImagePickerViewModel.swift
+++ MVVMExample/MVVMExample/Page/MVVMExamples/ImagePicker/ViewModel/ImagePickerViewModel.swift
@@ -13,16 +13,31 @@ import RxSwift
 import Action
 
 class ImagePickerViewModel: BaseViewModel {
+    
+    let rxTitle = BehaviorRelay<String?>(value: "")
+    let rxDescription = BehaviorRelay<String?>(value: "What's on your mind?")
     let rxImage = BehaviorRelay<UIImage?>(value: nil)
+    let rxCanPost = BehaviorRelay(value: false)
+
+    lazy var postAction: Action<Void, Void> = {
+        return Action(enabledIf: self.rxCanPost.asObservable()) {
+            return .just(self.post())
+        }
+    }()
+    
     
     override func react() {
         super.react()
         rxImage.subscribe(onNext: { (image) in
             
         }) => disposeBag
+        
+        Observable.combineLatest(rxTitle, rxDescription, rxImage) { title, desc, image in
+            return !(title?.isEmpty ?? false) && !(desc?.isEmpty ?? false) && (image != nil)
+        } ~> rxCanPost => disposeBag
     }
     
-    private func addImage() {
-        openCameraRoll()
+    private func post() {
+        
     }
 }
