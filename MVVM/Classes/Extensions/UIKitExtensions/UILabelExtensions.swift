55bfcea1ef220af5d56954ea7cc21b227417d1fc
Pham Minh Tien <phamminhtien305@gmail.com>
2020-11-30 21:55:54 +0700

    Fix UIlabel with html tag & fit image

--- MVVM/Classes/Extensions/UIKitExtensions/UILabelExtensions.swift
+++ MVVM/Classes/Extensions/UIKitExtensions/UILabelExtensions.swift
@@ -34,29 +34,47 @@ public extension UILabel {
     }
     
     
-    public func setHTMLFromString(htmlText: String, withTextHexColor hex: String = "929292") {
+    public func setHTMLFromString(htmlText: String, maxWidth: CGFloat, withTextHexColor hex: String = "929292") {
         let modifiedFont = String(format:"<span style=\"font-family: '-apple-system', 'HelveticaNeue'; font-size: \(self.font!.pointSize)\">%@</span>", htmlText)
 
         let attrStr = try! NSAttributedString(
             data: modifiedFont.data(using: .unicode, allowLossyConversion: true)!,
             options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding:String.Encoding.utf8.rawValue],
             documentAttributes: nil)
-        self.attributedText = attrStr
+        self.attributedText = attrStr.attributedStringWithResizedImages(with: maxWidth)
         self.textColor = UIColor(hexString: hex)
     }
     
-    public func setHTMLFromStringWithSettingFontSize(htmlText: String, withTextHexColor hex: String = "929292") {
-        var font = self.font!.pointSize
-        if let fontSize = UserDefaults.standard.value(forKey: "FontSize") as? CGFloat {
-            font = fontSize
-        }
-        let modifiedFont = String(format:"<span style=\"font-family: '-apple-system', 'HelveticaNeue'; font-size: \(font)\">%@</span>", htmlText)
+}
 
-        let attrStr = try! NSAttributedString(
-            data: modifiedFont.data(using: .unicode, allowLossyConversion: true)!,
-            options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding:String.Encoding.utf8.rawValue],
-            documentAttributes: nil)
-        self.attributedText = attrStr
-        self.textColor = UIColor(hexString: hex)
+
+extension NSAttributedString {
+    func attributedStringWithResizedImages(with maxWidth: CGFloat) -> NSAttributedString {
+        let text = NSMutableAttributedString(attributedString: self)
+        text.enumerateAttribute(NSAttributedString.Key.attachment, in: NSMakeRange(0, text.length), options: .init(rawValue: 0), using: { (value, range, stop) in
+            if let attachement = value as? NSTextAttachment {
+                let image = attachement.image(forBounds: attachement.bounds, textContainer: NSTextContainer(), characterIndex: range.location)!
+                if image.size.width > maxWidth {
+                    let newImage = image.resizeImage(scale: maxWidth/image.size.width)
+                    let newAttribut = NSTextAttachment()
+                    newAttribut.image = newImage
+                    text.addAttribute(NSAttributedString.Key.attachment, value: newAttribut, range: range)
+                }
+            }
+        })
+        return text
+    }
+}
+
+extension UIImage {
+    func resizeImage(scale: CGFloat) -> UIImage {
+        let newSize = CGSize(width: self.size.width*scale, height: self.size.height*scale)
+        let rect = CGRect(origin: CGPoint.zero, size: newSize)
+
+        UIGraphicsBeginImageContext(newSize)
+        self.draw(in: rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage!
     }
 }
